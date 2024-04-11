48b231c956b6f3a1fe5da1357745768bccc91ab2
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2024-04-11 13:40:19 +0400

    some packages are added

---
+++ Controllers/PatientLoginController.cs
@@ -0,0 +1,57 @@
+﻿using MVCprojectHealthCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCprojectHealthCare.Controllers
+{
+    public class PatientLoginController : Controller
+    {
+        public ActionResult Index(PatientLogin pl)
+        {
+            string connection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(connection);
+            string sqlQuery = "select email,password from [dbo].[patients] where email=@email and password=@password";
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(sqlQuery, sqlConnection);
+            if (pl.Email != null)
+            {
+                cmd.Parameters.AddWithValue("@email", pl.Email);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@email", DBNull.Value);
+            }
+            if (pl.Email != null)
+            {
+                cmd.Parameters.AddWithValue("@password", pl.Password);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@password", DBNull.Value);
+            }
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Session["email"] = pl.Email.ToString();
+                return RedirectToAction("welcome");
+            }
+            else
+            {
+                ViewData["Message"] = "User Login Details Failed";
+            }
+            sqlConnection.Close();
+            return View();
+        }
+        public ActionResult welcome()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Models/PatientLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCprojectHealthCare.Models
+{
+    public class PatientLogin
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Enter email: ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        [Display(Name = "Enter password: ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+    
+}
